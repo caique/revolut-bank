8c8d640c353177b84d3ed7d65070429807e5d850
Caique Rodrigues <caiquepeixoto1@gmail.com>
2019-06-18 02:53:01 -0300

    Fix AccountDetailsResponseBodyTest.

--- src/test/java/com/revolut/bank/api/accounts/responses/AccountDetailsResponseBodyTest.java
+++ src/test/java/com/revolut/bank/api/accounts/responses/AccountDetailsResponseBodyTest.java
@@ -1,27 +1,20 @@
 package com.revolut.bank.api.accounts.responses;
 
 import com.revolut.bank.services.domain.Account;
+import com.revolut.bank.services.domain.MoneyAmount;
 import org.junit.Test;
 
-import static com.revolut.bank.api.accounts.responses.AccountDetailsResponseBody.NUMBER_OF_DECIMAL_PLACES;
-import static com.revolut.bank.api.accounts.responses.AccountDetailsResponseBody.ROUNDING_STRATEGY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountDetailsResponseBodyTest {
 
     @Test
     public void returnsSameDetailsAsTheAccountProvidedInTheConstructor() {
-        Account account = new Account("john.doe@email.com");
+        Account account = new Account("john.doe@email.com", MoneyAmount.GRAND);
         AccountDetailsResponseBody accountDetailsResponseBody = new AccountDetailsResponseBody(account);
 
-        String expectedEmail = account.getEmail();
-        String expectedBalance = account
-                .getBalance()
-                .getValue()
-                .setScale(NUMBER_OF_DECIMAL_PLACES, ROUNDING_STRATEGY).toString();
-
-        assertThat(accountDetailsResponseBody.getEmail()).isEqualTo(expectedEmail);
-        assertThat(accountDetailsResponseBody.getBalance()).isEqualTo(expectedBalance);
+        assertThat(accountDetailsResponseBody.getEmail()).isEqualTo("john.doe@email.com");
+        assertThat(accountDetailsResponseBody.getBalance()).isEqualTo("100.00");
     }
 
 }
